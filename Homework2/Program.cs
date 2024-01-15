d19d9c256b3e21c6e4fc8181445c9110fd9aaaf9
Mashkabond <mashkabond23@gmail.com>
2024-01-15 23:54:11 +0300

    Changes after review

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace Homework2
@@ -6,8 +7,11 @@ namespace Homework2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        const int MaxElementCount = 1_000_000;
+        const int SearchElement = 496_752;
+        const int Divider = 777;
 
+        static void Main(string[] args)
         {
             var list = new List<int>();
             var arraylist = new ArrayList();
@@ -15,11 +19,95 @@ namespace Homework2
 
             var random = new Random();
 
-
             Stopwatch stopwatch = new Stopwatch();
+
+            MeasureCollectionsFilling(list, arraylist, linkedlist, random, stopwatch);
+
+            MeasureCollectionsElementSearch(list, arraylist, linkedlist, stopwatch);
+
+            MeasureCollectionsElementSearchByDivider(list, arraylist, linkedlist, stopwatch);
+        }
+
+        private static void MeasureCollectionsElementSearchByDivider(List<int> list, ArrayList arraylist, LinkedList<int> linkedlist, Stopwatch stopwatch)
+        {
+            int result = 0;
+
+            stopwatch.Restart();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                result = list[i];
+                if (result % Divider == 0)
+                    Console.WriteLine(result);
+
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Время, затраченное на поиск и вывод элементов, которые без остатка делятся на {Divider}, в List : {stopwatch.ElapsedMilliseconds} ms");
+
+            stopwatch.Restart();
+
+            for (int i = 0; i < arraylist.Count; i++)
+            {
+                result = (int)arraylist[i];
+                if (result % Divider == 0)
+                    Console.WriteLine(result);
+
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Время, затраченное на поиск и вывод элементов, которые без остатка делятся на {Divider}, в ArrayList : {stopwatch.ElapsedMilliseconds} ms");
+
+            stopwatch.Restart();
+
+            foreach (var number in linkedlist)
+            {
+
+                if (number % Divider == 0)
+                    Console.WriteLine(number);
+
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Время, затраченное на поиск и вывод элементов, которые без остатка делятся на {Divider}, в LinkedList : {stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        private static void MeasureCollectionsElementSearch(List<int> list, ArrayList arraylist, LinkedList<int> linkedlist, Stopwatch stopwatch)
+        {
+            var result = 0;
+            stopwatch.Restart();
+
+            result = list[SearchElement];
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Время, затраченное на поиск {SearchElement}-го элемента в List : {stopwatch.ElapsedMilliseconds} ms");
+
+            stopwatch.Restart();
+
+            result = (int)arraylist[SearchElement];
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Время, затраченное на поиск числа {SearchElement}-го элемента в ArrayList : {stopwatch.ElapsedMilliseconds} ms");
+
+            stopwatch.Restart();
+
+            result = linkedlist.ElementAt(SearchElement);
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Время, затраченное на поиск числа {SearchElement}-го элемента в LinkedList : {stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        private static void MeasureCollectionsFilling(List<int> list, ArrayList arraylist, LinkedList<int> linkedlist, Random random, Stopwatch stopwatch)
+        {
             stopwatch.Start();
 
-            for (int i = 0; i <= 1000000; i++)
+            for (int i = 0; i <= MaxElementCount; i++)
             {
                 list.Add(random.Next());
 
@@ -29,11 +117,9 @@ namespace Homework2
 
             Console.WriteLine($"Время, затраченное на добавление чисел в List : {stopwatch.ElapsedMilliseconds} ms");
 
+            stopwatch.Restart();
 
-            stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            for (int i = 0; i <= 1000000; i++)
+            for (int i = 0; i <= MaxElementCount; i++)
             {
                 arraylist.Add(random.Next());
 
@@ -43,11 +129,9 @@ namespace Homework2
 
             Console.WriteLine($"Время, затраченное на добавление чисел в ArrayList : {stopwatch.ElapsedMilliseconds} ms");
 
+            stopwatch.Restart();
 
-            stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            for (int i = 0; i <= 1000000; i++)
+            for (int i = 0; i <= MaxElementCount; i++)
             {
 
                 linkedlist.AddLast(random.Next());
@@ -57,85 +141,6 @@ namespace Homework2
             stopwatch.Stop();
 
             Console.WriteLine($"Время, затраченное на добавление чисел в LinkedList : {stopwatch.ElapsedMilliseconds} ms");
-
-
-            int result = 0;
-            stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            result = list[496752];
-
-            stopwatch.Stop();
-
-            Console.WriteLine($"Время, затраченное на поиск 496753-го элемента в List : {stopwatch.ElapsedMilliseconds} ms");
-
-
-            stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            result = (int)arraylist[496752];
-
-            stopwatch.Stop();
-
-            Console.WriteLine($"Время, затраченное на поиск числа 496753-го элемента в ArrayList : {stopwatch.ElapsedMilliseconds} ms");
-
-
-            stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            result = linkedlist.ElementAt(496752);
-
-            stopwatch.Stop();
-
-            Console.WriteLine($"Время, затраченное на поиск числа 496753-го элемента в LinkedList : {stopwatch.ElapsedMilliseconds} ms");
-
-
-            stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            for (int i = 0; i < list.Count; i++)
-            {
-                result = list[i];
-                if (result % 777 == 0)
-                    Console.WriteLine(result);
-
-            }
-
-            stopwatch.Stop();
-
-            Console.WriteLine($"Время, затраченное на поиск и вывод элементов, которые без остатка делятся на 777, в List : {stopwatch.ElapsedMilliseconds} ms");
-
-
-            stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            for (int i = 0; i < arraylist.Count; i++)
-            {
-                result = (int)arraylist[i];
-                if (result % 777 == 0)
-                    Console.WriteLine(result);
-
-            }
-
-            stopwatch.Stop();
-
-            Console.WriteLine($"Время, затраченное на поиск и вывод элементов, которые без остатка делятся на 777, в ArrayList : {stopwatch.ElapsedMilliseconds} ms");
-
-
-            stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            for (int i = 0; i < linkedlist.Count; i++)
-            {
-                result = linkedlist.ElementAt(i);
-                if (result % 777 == 0)
-                    Console.WriteLine(result);
-
-            }
-
-            stopwatch.Stop();
-
-            Console.WriteLine($"Время, затраченное на поиск и вывод элементов, которые без остатка делятся на 777, в LinkedList : {stopwatch.ElapsedMilliseconds} ms");
         }
     }
 }
