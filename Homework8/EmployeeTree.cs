97094232cf5e19e90ee0be351b9aacce3bae2df7
Mashkabond <mashkabond23@gmail.com>
2024-04-07 14:45:32 +0400

    Added Homework8

---
+++ Homework8/EmployeeTree.cs
@@ -0,0 +1,101 @@
+﻿namespace Homework8
+{
+    public class EmployeeTree
+    {
+        private EmployeeTreeNode? _root;
+
+        public void Traversal()
+        {
+            Traversal(_root);
+        }
+
+        public string? FindEmployeeNameBySalary(int salary) 
+        {
+            var treeNode = FindTreeNodeBySalary(_root, salary);
+            return treeNode?.Name;
+        }
+
+        public void Add(string name, int salary)
+        {
+            var employeeTreeNode = new EmployeeTreeNode(name, salary);
+            if (_root == null)
+            {
+                _root = employeeTreeNode;
+            }
+            else
+            {
+                AddTreeNode(_root, employeeTreeNode);
+            }
+        }
+
+        public void Clear()
+        {
+            _root = null;
+        }
+
+        private void Traversal(EmployeeTreeNode? root)
+        {
+            if (root?.Left != null) Traversal(root.Left);
+            Console.WriteLine(root);
+            if (root?.Right != null) Traversal(root.Right);
+        }
+
+        private EmployeeTreeNode? FindTreeNodeBySalary(EmployeeTreeNode? root, int salary)
+        {
+            if (salary < root?.Salary)
+            {
+                return root.Left != null ? FindTreeNodeBySalary(root.Left, salary) : null;
+            }
+            else if (salary > root?.Salary)
+            {
+                return root.Right != null ? FindTreeNodeBySalary(root.Right, salary) : null;
+            }
+            return root;
+        }
+
+        private void AddTreeNode(EmployeeTreeNode root, EmployeeTreeNode node)
+        {
+            if (node.Salary < root.Salary)
+            {
+                if (root.Left == null)
+                {
+                    root.Left = node;
+                }
+                else
+                {
+                    AddTreeNode(root.Left, node);
+                }
+            }
+            else
+            {
+                if (root.Right == null)
+                {
+                    root.Right = node;
+                }
+                else
+                {
+                    AddTreeNode(root.Right, node);
+                }
+            }
+        }
+
+        private class EmployeeTreeNode
+        {
+            public string Name { get;}
+            public int Salary { get;}
+            public EmployeeTreeNode? Left { get; internal set; }
+            public EmployeeTreeNode? Right { get; internal set; }
+
+            public EmployeeTreeNode(string name, int salary)
+            {
+                Name = name;
+                Salary = salary;
+            }
+
+            public override string ToString()
+            {
+                return $"{Name} - {Salary}";
+            }
+        }
+    }
+}
---
+++ Homework8/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Homework8
+{
+    internal class Program
+    {
+        private static EmployeeTree _tree = new EmployeeTree();
+        private static int _userEmployeeSalaryInput;
+        private static int _userActionInput;
+
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            while (true) 
+            {
+                if (_userActionInput == 0)
+                {
+                    _tree.Clear();
+                    StartUserEmployeesInput();
+                    _tree.Traversal();
+                    Console.WriteLine();
+                }
+                StartUserEmployeeSalaryInput();
+                var employeeName = _tree.FindEmployeeNameBySalary(_userEmployeeSalaryInput);
+                if (employeeName != null)
+                {
+                    Console.WriteLine($"Найден сотрудник с такой зарплатой - {employeeName}");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Такой сотрудник не найден");
+                    Console.WriteLine();
+                }
+                StartUserActionInput();
+            }
+        }
+
+        static void StartUserEmployeesInput() 
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите имя сотрудника:");
+                var employeeName = Console.ReadLine() ?? string.Empty;
+                if (employeeName == string.Empty) break;
+
+                Console.WriteLine("Введите ЗП сотрудника:");
+                int employeeSalary;
+                while (!int.TryParse(Console.ReadLine(), out employeeSalary))
+                {
+                    Console.WriteLine("Необходимо ввести числовое значение. Повторите ввод:");
+                }
+                _tree.Add(employeeName, employeeSalary);
+                Console.WriteLine();
+            }
+        }
+
+        static void StartUserEmployeeSalaryInput()
+        {
+            Console.WriteLine("Введите интересующую зарплату:");
+            while (!int.TryParse(Console.ReadLine(), out _userEmployeeSalaryInput))
+            {
+                Console.WriteLine("Необходимо ввести числовое значение. Повторите ввод:");
+            }
+        }
+
+        static void StartUserActionInput()
+        {
+            while (true)
+            {
+                Console.WriteLine("Выберите следующее действие:");
+                Console.WriteLine("0: Начать сначала");
+                Console.WriteLine("1: Искать сотрудника по зарплате");
+
+                var userActionInput = Console.ReadLine();
+                if (userActionInput == "0" || userActionInput == "1")
+                {
+                    _userActionInput = int.Parse(userActionInput);
+                    Console.WriteLine();
+                    break;
+                }
+            }
+        }
+    }
+}
