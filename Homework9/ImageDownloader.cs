ffa78ecb0b373489eeaf699e0e0a9660c02b1368
Mashkabond <mashkabond23@gmail.com>
2024-04-09 21:38:52 +0400

    Added Homework9

---
+++ Homework9/ImageDownloader.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel;
+using System.Net;
+
+namespace Homework9
+{
+    internal class ImageDownloader
+    {
+        public event Action<string>? ImageStarted;
+        public event Action<string>? ImageCompleted;
+
+        private List<ImageDownload> _downloads = new();
+
+        public void DownloadAsync(params string[] urls)
+        {
+            foreach (var url in urls)
+            {
+                var download = new ImageDownload { Url = url };
+                StartDownloadTask(download);
+                _downloads.Add(download);
+            }
+        }
+
+        public void PrintImageDownloadStatuses()
+        {
+            foreach (var download in _downloads)
+            {
+                if (download.Task.IsCompleted)
+                {
+                    Console.Out.WriteLine($"{download.Filename} - успешно загружен!");
+                }
+                else
+                {
+                    Console.Out.WriteLine($"{download.Filename} - загружается...");
+                }
+            }
+            Console.Out.WriteLine();
+        }
+
+        private void StartDownloadTask(ImageDownload download) 
+        {
+            var webClient = new WebClient();
+            webClient.DownloadFileCompleted += OnDownloadFileCompleted(download);
+
+            download.Filename = Path.GetFileName(download.Url);
+
+            ImageStarted?.Invoke("Скачивание файла началось");
+            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", download.Filename, download.Url);
+            download.Task = webClient.DownloadFileTaskAsync(download.Url, download.Filename);
+        }
+
+        private AsyncCompletedEventHandler OnDownloadFileCompleted(ImageDownload download)
+        {
+            return (_, e) =>
+            {
+                if (e.Error != null)
+                {
+                    throw e.Error;
+                }
+                else
+                {
+                    Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", download.Filename, download.Url);
+                    ImageCompleted?.Invoke("Скачивание файла закончилось\n");
+                }
+            };
+        }
+
+        private class ImageDownload
+        {
+            public string Url { get; set; }
+            public string Filename { get; set; }
+            public Task Task { get; set; }
+        }
+    }
+}
---
+++ Homework9/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Homework9
+{
+    internal class Program
+    {
+
+        private static readonly string[] Images = new string[]
+        {
+            "https://webneel.com/daily/sites/default/files/images/daily/08-2018/1-nature-photography-spring-season-mumtazshamsee.jpg",
+            "https://webneel.com/daily/sites/default/files/images/daily/07-2021/2-sculpture-stairway-heaven-australia-david-mccracken.jpg",
+            "https://webneel.com/daily/sites/default/files/images/daily/07-2021/1-sculpture-stairway-heaven-australia-david-mccracken.jpg",
+            "https://webneel.com/daily/sites/default/files/images/daily/07-2021/6-sculpture-caring-hand-switzerland.jpg",
+            "https://webneel.com/daily/sites/default/files/images/daily/07-2021/7-sculpture-mother-nature-rotating-earth.jpg",
+            "https://webneel.com/daily/sites/default/files/images/daily/07-2021/9-sculpture-de-vaartkapoen-belgium-tom-frantzen.jpg",
+            "https://webneel.com/daily/sites/default/files/images/daily/07-2021/13-sculpture-fantasy-wire-fairies-trentham-garden-britain.jpg",
+            "https://webneel.com/daily/sites/default/files/images/daily/07-2021/12-sculpture-fantasy-wire-fairies-trentham-garden-britain.jpg",
+            "https://webneel.com/daily/sites/default/files/images/daily/07-2021/14-sculpture-mustangs-texas-usa-robert-glen.jpg",
+            "https://webneel.com/daily/sites/default/files/images/daily/07-2021/15-sculpture-mustangs-texas-usa-robert-glen.jpg"
+        };
+
+        static void Main(string[] args)
+        {
+            var downloader = new ImageDownloader();
+
+            downloader.ImageStarted += Console.WriteLine;
+            downloader.ImageCompleted += Console.WriteLine;
+
+            downloader.DownloadAsync(Images);
+
+            Console.WriteLine("Для выхода из программы нажмите 'A'. Для проверки статуса нажмите любую другую клавишу");
+            OnKeyPress(downloader);
+        }
+
+        static void OnKeyPress(ImageDownloader downloader)
+        {
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.A)
+                {
+                    break;
+                }
+                else
+                {
+                    downloader.PrintImageDownloadStatuses();
+                }
+            }
+        }
+    }
+}
