a41cbd152729e8c9bcdc93e63b75833485ab2c17
Mashkabond <mashkabond23@gmail.com>
2024-04-12 18:02:25 +0400

    Added Homework11

---
+++ Homework11/OtusDictionary.cs
@@ -0,0 +1,78 @@
+﻿namespace Homework11
+{
+    public class OtusDictionary
+    {
+        private DictionaryEntry[] _entries = new DictionaryEntry[16];
+
+        public string? this[int key]
+        {
+            get => Get(key);
+            set => Add(key, value);
+        }
+
+        public string? Get(int key)
+        {
+            var index = GetEntryIndexByKey(key);
+            var entry = _entries[index];
+            return entry?.Value;
+        }
+
+        public void Add(int key, string? value) 
+        {
+            if (value == null) throw new ArgumentNullException("value");
+
+            var index = GetEntryIndexByKey(key);
+            var entry = _entries[index];
+            if (entry == null) 
+            {
+                _entries[index] = new DictionaryEntry(key, value);
+            }
+            else if (entry.Key == key)
+            {
+                entry.Value = value;
+            }
+            else 
+            {
+                Console.WriteLine("Произошла коллизия, увеличиваю размер массива в 2 раза");
+                ResizeDictionary();
+            }
+        }
+
+        private int GetEntryIndexByKey(int key)
+        {
+            return 31 * Math.Abs(key) % _entries.Length;
+        }
+
+        private void ResizeDictionary()
+        {
+            var newSize = _entries.Length * 2;
+            if (newSize < int.MaxValue)
+            {
+                var resizedEntries = new DictionaryEntry[newSize];
+                foreach (var entry in _entries)
+                {
+                    if (entry != null)
+                    {
+                        Add(entry.Key, entry.Value);
+                    }
+                }
+                _entries = resizedEntries;
+            }
+            else 
+            {
+                throw new InvalidOperationException("Массив переполнен");
+            }
+        }
+
+        private class DictionaryEntry
+        {
+            public int Key { get; set; }
+            public string Value { get; set; }
+
+            public DictionaryEntry(int key, string value) {
+                Key = key;
+                Value = value;
+            }
+        }
+    }
+}
---
+++ Homework11/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Homework11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new OtusDictionary();
+
+            dictionary.Add(1, "s1");
+            dictionary.Add(2, "s2");
+            dictionary.Add(3, "s3");
+
+            Console.WriteLine(dictionary.Get(1));  // s1
+            Console.WriteLine(dictionary.Get(-1)); // s1
+            Console.WriteLine(dictionary.Get(2));  // s2
+            Console.WriteLine(dictionary.Get(3));  // s3
+
+            dictionary.Add(3, "s4");
+            Console.WriteLine(dictionary.Get(3)); // s4
+
+            Console.WriteLine(dictionary.Get(0)); // empty
+            Console.WriteLine(dictionary.Get(1000)); // empty
+
+            dictionary[10] = "test";
+            Console.WriteLine(dictionary[10]); // test
+
+            // ResizeDictionary
+            dictionary.Add(20, "s20");
+            dictionary.Add(36, "s36");
+
+            dictionary[3] = null; // ArgumentNullException
+        }
+    }
+}
