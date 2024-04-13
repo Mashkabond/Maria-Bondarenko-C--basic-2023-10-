788ef6b76ade5206facfaf090780488d14247576
Mashkabond <mashkabond23@gmail.com>
2024-04-13 12:09:25 +0400

    Added Homework13

---
+++ Homework13/EnumerableExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace Homework13
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> collection, int percent)
+        {
+            ValidatePercent(percent);
+            var limit = (int)Math.Ceiling(collection.Count() * percent / 100.0);
+            return (
+                from item in collection
+                orderby item descending
+                select item
+                   )
+                .Take(limit);
+        }
+
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> collection, int percent, Func<T, int> orderByFunc)
+        {
+            ValidatePercent(percent);
+            var limit = (int)Math.Ceiling(collection.Count() * percent / 100.0);
+            return collection
+                .OrderByDescending(orderByFunc)
+                .Take(limit);
+        }
+
+        private static void ValidatePercent(int percent)
+        {
+            if (percent < 1 || percent > 100)
+            {
+                throw new ArgumentException("Процент должен иметь значение от 1 до 100");
+            }
+        }
+    }
+}
---
+++ Homework13/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Homework13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine(string.Join(", ", numbers.Top(30).ToList()));
+
+            var people = new List<Person>()
+            {
+                new("Мария", 27),
+                new("Анна", 30),
+                new("Алексей", 26),
+                new("Екатерина", 35)
+            };
+            Console.WriteLine(string.Join("\n", people.Top(30, person => person.Age).ToList()));
+        }
+
+        private class Person
+        {
+            public string Name { get; }
+            public int Age { get; }
+
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+
+            public override string ToString()
+            {
+                return $"Имя: {Name}, Возраст: {Age}";
+            }
+        }
+    }
+}
